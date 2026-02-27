7af0fa67b57c1f20809f5fb786ef7aae825e5b2c
otabayevamehribon1 <otaboyevamehribon505@gmail.com>
2026-02-27 15:42:28 +0500

    ikkinchi failim
    aa

---
+++ dars.dars.py
@@ -0,0 +1,10 @@
+# #20. Salom ber chiqarilsin.
+# ism = input("ism kiriting :")
+# print(f"Assalomu Alekum {ism.title()} Tanishganimdan Xursand man")
+
+
+
+def salom_ber(ism):
+    print(f"Assalomu Alekum, {ism} Yaxshimisiz!")
+
+salom_ber("Ilyosbek")
