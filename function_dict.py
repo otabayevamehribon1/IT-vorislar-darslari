d4d68b0e50b1abc74a0dd17f9d766e9dff43fb70
otabayevamehribon1 <otaboyevamehribon505@gmail.com>
2026-03-04 16:08:16 +0500

    lugat darsi
    mmm

---
+++ function_dict.py
@@ -0,0 +1,21 @@
+# davlatlar = {
+#     'UZB':"Toshkent",
+#     'TJ':"Dushanbe",
+#     'KG':"beshkek",
+#     'TM':"Oshqabot"
+# }
+# davlatlar['AQSH']='Washington'
+# davlatlar['Turkiya']="Anqara"
+# print(davlatlar)
+
+# Funksiyaga lugat berish
+# def talaba_bahosi(**talabalar):
+#     while True:
+#         baho= talabalar['baho']
+#         ism=talabalar['ism']
+#         return f"Hurmatli {ism} sizning bahongiz {baho} ekan !"
+# print(talaba_bahosi(ism="Ali",baho=5))
+
+
+
+
