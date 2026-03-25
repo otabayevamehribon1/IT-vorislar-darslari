fbfbb2f09b6457b8894592cb81befdb6d17873ac
Otaboyeva Mehribon <otaboyevamehribon505@gmail.com>
2026-03-25 16:12:52 +0500

    modullar
    sdf

---
+++ amaliyot.py
@@ -0,0 +1,29 @@
+# import math as m
+
+# ozgaruvchi = math.cos(0.2)
+# print(ozgaruvchi)
+
+# import def_operatori
+# ozgaruvchi1 = def_operatori.ishorani_aniqlovchi
+
+
+# import def_operatori
+
+
+# kopaytma = def_operatori.kopaytma(6,9,8,9)
+# print(kopaytma)
+
+
+# import def_operatori as m
+# daraja  = m.darajaga_oshirish(3,2)
+# print(daraja)
+
+
+from masalalar import ayirma, yigindi
+daraja = ayirma(23, 2)
+daraja = yigindi(56,7)
+
+
+
+
+
